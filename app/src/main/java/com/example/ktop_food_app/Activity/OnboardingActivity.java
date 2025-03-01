51bea8f652cfb66ea366ada7a7790ea90a869557
minhvh5504 <vihongminh5504@gmail.com>
2025-03-01 23:34:41 +0700

    feat: connect firebase

--- app/src/main/java/com/example/ktop_food_app/Activity/OnboardingActivity.java
+++ app/src/main/java/com/example/ktop_food_app/Activity/OnboardingActivity.java
@@ -1,6 +1,8 @@
 package com.example.ktop_food_app.Activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +11,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ktop_food_app.R;
+import com.example.ktop_food_app.databinding.ActivityOnboardingBinding;
 
-public class OnboardingActivity extends AppCompatActivity {
+public class OnboardingActivity extends FirebaseActivity {
+    ActivityOnboardingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onboarding);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityOnboardingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setVariable();
+        getWindow().setStatusBarColor(Color.parseColor("#F9C77A"));
+    }
+
+    private void setVariable() {
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Logic login button
+            }
+        });
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Logic signup button
+            }
         });
     }
 }
